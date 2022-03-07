bdfe4c18f7a5ff1cbd935f38cac7d5b0fc811345
Documenter.jl <documenter@juliadocs.github.io>
2022-03-07 17:53:38 +0000

    build based on fc97bfa

--- dev/search_index.js
+++ dev/search_index.js
@@ -1,3 +1,3 @@
 var documenterSearchIndex = {"docs":
-[{"location":"","page":"Home","title":"Home","text":"CurrentModule = Term","category":"page"},{"location":"","page":"Home","title":"Home","text":"import Term: make_logo\nlogo = make_logo()\n\nprint(logo)","category":"page"}]
+[{"location":"","page":"Home","title":"Home","text":"CurrentModule = Term","category":"page"},{"location":"","page":"Home","title":"Home","text":"import Term\nprint(Term.make_logo())","category":"page"}]
 }
