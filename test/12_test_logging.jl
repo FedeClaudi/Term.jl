856697c34df1a2d3246c907591db08542d9eb5e7
FedeClaudi <federicoclaudi@protonmail.com>
2022-03-14 23:10:50 +0000

    tests passing locally

--- test/12_test_logging.jl
+++ test/12_test_logging.jl
@@ -3,29 +3,29 @@ import Term: install_term_logger, uninstall_term_logger
 install_term_logger()
 
 println("\nTesting logging, stdout temporarily disabled")
-# @suppress_out begin
-@testset "\e[34mLOGS test" begin
-    @test_nothrow @info "my log!"
+@suppress_out begin
+    @testset "\e[34mLOGS test" begin
+        @test_nothrow @info "my log!"
 
-    @test_nothrow @warn "tell us if this was [bold red]undexpected![/bold red]"
+        @test_nothrow @warn "tell us if this was [bold red]undexpected![/bold red]"
 
-    x = collect(1:2:20)
-    y = x * x'
-    name = "the name is [bold blue]Term[/bold blue]"
-    p1 = Panel("text")
+        x = collect(1:2:20)
+        y = x * x'
+        name = "the name is [bold blue]Term[/bold blue]"
+        p1 = Panel("text")
 
-    @test_nothrow @error "[italic green bold]fancy logs![/italic green bold]" x y name √9 install_term_logger p1
+        @test_nothrow @error "[italic green bold]fancy logs![/italic green bold]" x y name √9 install_term_logger p1
 
-    @test_nothrow @info """asdada asdasd\nasdada;
-    asdadaada
-    asdadaxc
+        @test_nothrow @info """asdada asdasd\nasdada;
+        asdadaada
+        asdadaxc
 
-    sdfs
-    s""" 1 + 2
+        sdfs
+        s""" 1 + 2
 
-    uninstall_term_logger()
+        uninstall_term_logger()
 
-    @info "removed"
+        @info "removed"
 
+    end
 end
-# end
