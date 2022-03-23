eb5d7b110cb84d6a2eba03c53963850903830fee
FedeClaudi <federicoclaudi@protonmail.com>
2022-03-23 23:08:52 +0000

    respacing working for simple text

--- scripts/txt_reshaping.jl
+++ scripts/txt_reshaping.jl
@@ -1,4 +1,4 @@
-import Term: reshape_text, RenderableText, replace_ansi, ANSI_REGEXEs, get_last_ANSI_code, loop_last, textlen
+import Term: reshape_text, RenderableText, replace_ansi, ANSI_REGEXEs, get_last_ANSI_code, loop_last, textlen, has_markup, has_ansi
 import Term.style: apply_style
 
 struct AnsiTag
@@ -6,9 +6,25 @@ struct AnsiTag
     stop::Int
 end
 
+# ------------------------------ create reshaped ----------------------------- #
+function create_reshaped_text_simple(text, cuts)
+    lines = ""
+    for (last, (pre, post)) in loop_last(zip(cuts[1:end-2], cuts[2:end-1]))
+        newline = text[pre:post]
+        ansi = get_last_ANSI_code(newline)
+
+        if last
+            lines *= lstrip(newline)*"\e[0m"
+        else
+            lines *= lstrip(newline)*"\e[0m\n"*ansi
+        end
+    end
+    return chomp(lines)
+end
 
 
-function create_reshaaped_text(text, cuts)
+
+function create_reshaped_text_complex(text, cuts)
     lines = ""
     for (last, (pre, post)) in loop_last(zip(cuts[1:end-2], cuts[2:end-1]))
         pre = isvalid(text, pre) ? pre : prevind(text, pre)
@@ -26,59 +42,56 @@ function create_reshaaped_text(text, cuts)
     return chomp(lines)
 end
 
-function test(text, width)
-    text = apply_style(text)
 
-    # get which chars are in a tag and which ones are not
-    tags = map(m -> AnsiTag(
-                        max(m.offset-1, 1), 
-                        m.offset+textwidth(m.match)-1
-                    ), eachmatch(ANSI_REGEXEs[1], text))
 
-    in_tag = ones(Int, length(text))
-    for tag in tags
-        in_tag[tag.start:tag.stop] .= 0
-    end
 
-    # get a measure of the text width at each char
-    nunits = cumsum(ncodeunits.(collect(text)) .*  in_tag)
-    widths = cumsum(textwidth.(collect(text)) .*  in_tag)
+
+# --------------------------------- get cuts --------------------------------- #
+
+# function getcuts_simple_nospaces(text, width, nunits, widths)
+#     spaces = 
+# end
+
+function getcuts_simple(text, width, nunits)
+    # !occursin(' ', text) && return getcuts_simple_nospaces(text, width, nunits, widths)
 
     # get the nunits at each space
-    spaces = findall(' ', text)
+    spaces = findall(' ', text) # in n units, not char idxcc
 
     # if the text between spaces is too long, add extra spaces
     Δ = 0
     if length(spaces) == 0
-        Δ = -1
-        spaces = findall(diff(mod.(widths, width)) .<= 1)[2:end]
+        Δ = 1
+        spaces = findall(diff(mod.(nunits, width)) .<= 1)[2:end]
     end
 
     # get the width at each space
     nspaces = length(spaces)
-    space_widths = [1, widths[spaces]..., textwidth(text)]
+    space_widths = [1, nunits[spaces]..., length(text)]
 
     # get cuts locations
     addcut(c) = push!(cuts, c)
     cuts::Vector{Int} = [1]
     _space_widths = zeros(Int, length(space_widths))
+
     while cuts[end] < textwidth(text)
-        _space_widths[:] = space_widths .- nunits[cuts[end]] .- Δ
-        selected = findlast(0 .<= _space_widths .<= width)
-
-        if isnothing(selected) 
-            break
+        if Δ == 0  # there were spaces originally
+            _space_widths[:] = space_widths .- nunits[cuts[end]]
+            selected = findlast(0 .<= _space_widths .< width)
+        else
+            _space_widths[:] = space_widths .- nunits[cuts[end]] .+ Δ
+            selected = findlast(0 .<= _space_widths .<= width)
         end
+        isnothing(selected) && break
 
-        if selected > nspaces
-            _cut = max(nextind(text, cuts[end]), 1)
-            _candidate = text[_cut:end]
-            if textlen(_candidate) > width && nspaces > 0
+        if selected > nspaces && nspaces > 0
+            _candidate = text[cuts[end]:end]
+            if textlen(_candidate) > width 
                 addcut(spaces[end])
-                addcut(textwidth(text))
-            else
-                addcut(textwidth(text))
             end
+            break
+        elseif selected == 1
+            # @info space_widths cuts selected 
             break
         else
             addcut(spaces[selected-1])
@@ -87,40 +100,126 @@ function test(text, width)
     addcut(textwidth(text))
  
     # cut text
-    return create_reshaaped_text(text, cuts)
+    return cuts
 end
 
 
-text = "my text is \e[31mred for a while \e[39mand then it is \e[34malso blue!\e[39m\e[39m"^1
+function test(text, width)
+    issimple = length(text) == ncodeunits(text)
+
+    if has_markup(text) || has_ansi(text)
+        text = apply_style(text)
+
+        # get which chars are in a tag and which ones are not
+        tags = map(m -> AnsiTag(
+                            max(m.offset-1, 1), 
+                            m.offset+textwidth(m.match)-1
+                        ), eachmatch(ANSI_REGEXEs[1], text))
+
+        in_tag = ones(Int, length(text))
+        for tag in tags
+            in_tag[tag.start:tag.stop] .= 0
+        end
+
+        # get a measure of the text width at each char
+        nunits = cumsum(ncodeunits.(collect(text)) .*  in_tag)
+    else
+        nunits = cumsum(ncodeunits.(collect(text)))
+    end
+
+    if issimple
+        cuts = getcuts_simple(text, width, nunits)
+        return create_reshaped_text_simple(text, cuts)
+    else
+        widths = cumsum(textwidth.(collect(text)) .*  in_tag)
+        cuts = getcuts_simple(text, width, nunits, widths)
+    end
+end
+
+text = "my text is very simple it doesn't have any style information"
+# text = "my text is \e[31mred for a while \e[39mand then it is \e[34malso blue!\e[39m\e[39m"^1
 # text = "[red]red[/red] and [bold black underline on_green]not[/bold black underline on_green] "^5
-text = "."^100
+# text = "."^100
+
 # text = "┌────────────────┬────────────────┬────────────────┬────────────────┬──────────────"
-text = "나랏말싸미 듕귁에  달아나랏 말싸미 듕귁에 달아 나랏말싸 미 듕귁에 달아 나랏말싸미 듕귁에 달아 나랏말싸미 듕귁에 달아"
+# text = "나랏말싸미 듕귁에  달아나랏 말싸미 듕귁에 달아 나랏말싸 미 듕귁에 달아 나랏말싸미 듕귁에 달아 나랏말싸미 듕귁에 달아"
+# text = "나 랏 말 싸 미 듕 귁 에 달 아 나 랏 말 싸 미 듕 귁 에 나 랏 말 싸 미 듕 귁 에 달 아 나 랏 말 싸 미 듕 귁 에 "
 
 print("\n"^3)
 for w in (9, 15, 22)
     println('_'^w)
     # println(t)
-    println(test(text, w))
+    @time println(test(text, w))
     # print('.'^w)
     # @time test(text, w)
 
 end
 
 
-# text = "my text is [red]red for a while [/red]and then it is [blue]also blue![/blue] "
-
-# # for w in (10, 14, 19, 28, 36, 41, 60)
-# #     println("."^w)
-# #     println(reshape_text(text, w))
-# # end
 
 
-# # TODO look at RenderableText
-# tt = text^5
 
-# for w in (10, 14, 19, 28, 36, 41, 60)
-#     println("."^w)
-#     println(RenderableText(tt; width=w))
-#     @time RenderableText(tt; width=w)
+# text = apply_style(text)
+
+# # get which chars are in a tag and which ones are not
+# tags = map(m -> AnsiTag(
+#                     max(m.offset-1, 1), 
+#                     m.offset+textwidth(m.match)-1
+#                 ), eachmatch(ANSI_REGEXEs[1], text))
+
+# in_tag = ones(Int, length(text))
+# for tag in tags
+#     in_tag[tag.start:tag.stop] .= 0
 # end
+
+# # get a measure of the text width at each char
+# nunits = cumsum(ncodeunits.(collect(text)) .*  in_tag)
+# widths = cumsum(textwidth.(collect(text)) .*  in_tag)
+# idxs = cumsum(length.(collect(text)) .*  in_tag)  # idx of each valid char
+
+# # get the nunits at each space
+# spaces = findall(' ', text) # in n units, not char idxcc
+
+# # if the text between spaces is too long, add extra spaces
+# Δ = 0
+# if length(spaces) == 0
+#     Δ = -1
+#     spaces = findall(diff(mod.(widths, width)) .<= 1)[2:end]
+# end
+
+# # get the width at each space
+# nspaces = length(spaces)
+# swidth(space) = widths[length(text[1:space])]  # get the width of the text up to the space
+# sidx(space) = idxs[length(text[1:space])]
+# space_widths = [1, swidth.(spaces)..., textwidth(text)]
+
+# # get cuts locations
+# addcut(c) = push!(cuts, c)
+# cuts::Vector{Int} = [1]
+# _space_widths = zeros(Int, length(space_widths))
+# while cuts[end] < textwidth(text)
+#     _space_widths[:] = space_widths .- nunits[cuts[end]] .- Δ
+#     selected = findlast(0 .<= _space_widths .<= width)
+
+#     if isnothing(selected) 
+#         break
+#     end
+
+#     if selected > nspaces
+#         _cut = max(nextind(text, cuts[end]), 1)
+#         _candidate = text[_cut:end]
+#         if textlen(_candidate) > width && nspaces > 0
+#             addcut(spaces[end])
+#             addcut(textwidth(text))
+#         else
+#             addcut(textwidth(text))
+#         end
+#         break
+#     else
+#         addcut(spaces[selected-1])
+#     end
+# end
+# addcut(textwidth(text))
+
+# # cut text
+# return create_reshaped_text(text, cuts)
