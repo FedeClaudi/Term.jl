ca4b754df47f2826080b87b807cdcb5a5e301ed9
FedeClaudi <federicoclaudi@protonmail.com>
2022-03-23 16:59:13 +0000

    text reshaping wip

--- scripts/txt_reshaping.jl
+++ scripts/txt_reshaping.jl
@@ -1,5 +1,5 @@
 import Term: reshape_text, RenderableText, replace_ansi, ANSI_REGEXEs, get_last_ANSI_code, loop_last, textlen
-
+import Term.style: apply_style
 
 struct AnsiTag
     start::Int
@@ -26,14 +26,12 @@ function get_putative_cuts(text, width)
 end
 
 
-function test(width)
-    text = "my text is \e[31mred for a while \e[39mand then it is \e[34malso blue!\e[39m\e[39m"^1
-
-
+function test(text, width)
+    text = apply_style(text)
 
     # get which chars are in a tag and which ones are not
     tags = map(m -> AnsiTag(
-                        m.offset-1, 
+                        max(m.offset-1, 1), 
                         m.offset+textwidth(m.match)-1
                     ), eachmatch(ANSI_REGEXEs[1], text))
 
@@ -44,39 +42,76 @@ function test(width)
 
     # get a measure of the text width at each char
     nunits = cumsum(ncodeunits.(collect(text)) .*  in_tag)
+    widths = cumsum(textwidth.(collect(text)) .*  in_tag)
+    # @warn nunits
+    # @info widths
 
     # get the nunits at each space
     spaces = findall(' ', text)
+
+    # if the text between spaces is too long, add extra spaces
+    Δ = 0
+    if length(spaces) == 0
+        Δ = -1
+        spaces = findall(diff(mod.(widths, width)) .<= 1)[2:end]
+    else
+        # throw("not implemented")
+        # for n in 2:length(space_widths)
+        #     if (space_widths[n]-space_widths[n-1] > width
+
+        #     end
+        # end
+    end
+
+    # get the width at each space
     nspaces = length(spaces)
+    # @info nunits
+    # @info text
     space_widths = [1, nunits[spaces]..., textwidth(text)]
 
+    # @info nunits
+    # @info space_widths "space widths"
+    # @info "spaces" spaces
+
     # get cuts at spaces
+    # addcut(c) = c < length(text) ? push!(cuts, nextind(text, c)) : push!(cuts, prevind(text, c))
+    addcut(c) = push!(cuts, c)
+
     cuts::Vector{Int} = [1]
     _space_widths = zeros(Int, length(space_widths))
     while cuts[end] < textwidth(text)
-        _space_widths[:] = space_widths .- nunits[cuts[end]]
-        selected = findlast(0 .<= _space_widths .< width)
-        isnothing(selected) && break
+        _space_widths[:] = space_widths .- nunits[cuts[end]] .- Δ
+        selected = findlast(0 .<= _space_widths .<= width)
+
+        if isnothing(selected) 
+            println(space_widths)
+            println(_space_widths)
+            break
+        end
 
         if selected > nspaces
             _candidate = text[cuts[end]:end]
-            if textlen(_candidate) > width
-                push!(cuts, spaces[end])
-                push!(cuts, textwidth(text))
+            if textlen(_candidate) > width && nspaces > 0
+                addcut(spaces[end])
+                addcut(textwidth(text))
             else
-                push!(cuts, textwidth(text))
+                addcut(textwidth(text))
             end
             break
         else
-            push!(cuts, spaces[selected-1])
+            addcut(spaces[selected-1])
         end
     end
-    push!(cuts, textwidth(text))
+    addcut(textwidth(text))
  
     # cut text
     lines = ""
-    for (last, (pre, post)) in loop_last(zip(cuts[1:end-1], cuts[2:end]))
-        newline = text[pre:post-1]
+    for (last, (pre, post)) in loop_last(zip(cuts[1:end-2], cuts[2:end-1]))
+        # _pre = max(nextind(text, pre), 1)
+        # _post = nextind(text, post)
+        pre = isvalid(text, pre) ? pre : prevind(text, pre)
+        post = isvalid(text, post) ? post : nextind(text, post)
+        newline = text[pre:post]
         ansi = get_last_ANSI_code(newline)
 
         if last
@@ -88,13 +123,20 @@ function test(width)
     return chomp(lines)
 end
 
+
+text = "my text is \e[31mred for a while \e[39mand then it is \e[34malso blue!\e[39m\e[39m"^1
+text = "[red]red[/red] and [bold black underline on_green]not[/bold black underline on_green] "^5
+text = "."^100
+text = "┌────────────────┬────────────────┬────────────────┬────────────────┬──────────────"
+text = "나랏말싸미 듕귁에 달아나랏말싸미 듕귁에 달아 나랏말싸미 듕귁에 달아 나랏말싸미 듕귁에 달아 나랏말싸미 듕귁에 달아"
+
 print("\n"^3)
-for w in (10, 15, 22)
+for w in (9, 15, 22)
     println('_'^w)
     # println(t)
-    # println(test(w))
+    println(test(text, w))
     # print('.'^w)
-    @time test(w)
+    @time test(text, w)
 
 end
 
