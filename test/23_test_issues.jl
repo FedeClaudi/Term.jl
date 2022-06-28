a05f711735d91b40168160c8db1a7afc24cb266b
t-bltg <tf.bltg@gmail.com>
2022-06-28 11:42:10 +0200

    fix issue 129

--- test/23_test_issues.jl
+++ test/23_test_issues.jl
@@ -1,3 +1,7 @@
 @testset "termshow fails on 1-by-1 matrices (#127)" begin
     @test termshow(IOBuffer(), hcat([1])) isa Nothing
 end
+
+@testset "termshow fails on datatypes (#129)" begin
+    @test termshow(IOBuffer(), Int) isa Nothing
+end
