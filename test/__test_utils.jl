6f7e3f59e56a1b7f5e0d23abf5f36287c3bdd64e
FedeClaudi <federicoclaudi@protonmail.com>
2022-08-23 17:26:07 -0400

    plis

--- test/__test_utils.jl
+++ test/__test_utils.jl
@@ -105,7 +105,7 @@ macro compare_to_string(obj, filename, fn = x -> x, skip=nothing)
         txt = $fn(txt)
 
         !isnothing($skip) && (
-            txt = join(split(txt, '\n')[1:end-$sskip], '\n')
+            txt = join(split(txt, '\n')[1:end-$skip], '\n')
         )
 
         filepath = "./txtfiles/$($filename).txt"
