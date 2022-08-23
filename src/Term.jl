b4b25c60058a21ffb5cd1322b343cf276a30f404
Mark Kittisopikul <markkitt@gmail.com>
2022-08-22 23:25:40 -0400

    Add a docstring for the Term module/package.

--- src/Term.jl
+++ src/Term.jl
@@ -1,3 +1,33 @@
+"""
+    Term.jl
+
+Welcome to Term.jl! Term.jl is a Julia library for producing styled, beautiful terminal output.
+
+# Documentation
+
+See https://fedeclaudi.github.io/Term.jl for documentation.
+
+# Demonstration
+
+```julia
+using Term
+const term_demo = joinpath(dirname(pathof(Term)), "..", "README.jl")
+include(term_demo) # view demo
+less(term_demo) # see demo code
+```
+
+# Example
+
+```jldoctest
+begin
+    println(@green "this is green")
+    println(@blue "and this is blue")
+    println()
+    println(@bold "this is bold")
+    println(@underline "and this is underlined")
+end
+```
+"""
 module Term
 
 const DEBUG_ON = Ref(false)
