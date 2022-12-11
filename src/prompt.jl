d8e0ddc65a3bb770027e87103efc26fc7e4ba247
FedeClaudi <federicoclaudi@protonmail.com>
2022-12-11 11:10:12 -0500

    bump

--- src/prompt.jl
+++ src/prompt.jl
@@ -6,7 +6,22 @@ using Term
 abstract type AbstractPrompt end
 
 """ display an `AbstractPrompt`, get user's reply and validate. """
-function ask end
+function ask(prompt::AbstractPrompt)
+    println(prompt)
+    ans = readline()
+    return validate_answer(ans, prompt)
+end
+
+"""
+    validate_answer
+
+Validate user's answer for a prompt type.
+The validation mechanism depends on the type of prompt.
+Validate answer will return the answer if it passed validation
+or raise and error otherwise.
+"""
+function validate_answer end
+
 
 # -------------------------------- type prompt ------------------------------- #
 """
@@ -17,12 +32,25 @@ function ask end
 
 Asks for input given `prompt` and checks/converts the answer to type `T`
 """
-struct TypePrompt{T}
-    answer_type::Union{Union, DataType} = T
+struct TypePrompt{T} <: AbstractPrompt
+    answer_type::T
     prompt::String
 end
 
-# TODO ask and validate answer methods and print
+function Base.println(io, prompt::TypePrompt)
+    println(io, prompt.prompt)
+end
+
+function validate_answer(answer, prompt::TypePrompt)
+    answer isa prompt.answer_type && return answer
+    try
+        return convert(prompt.answer_type, answer)
+    catch
+        error("Prompt expected answer of type: $(prompt.answer_type), got $(typeof(answer))")
+    end
+end
+
+
 
 # ---------------------------------------------------------------------------- #
 #                                OPTIONS PROMPTS                               #
@@ -44,4 +72,7 @@ end
 
 confirm() = ask(DefaultPropt(["Yes", "No"], 1, "Confirm?"))
 
+
+
+ask(TypePrompt(Int, "give me a number"))
 
