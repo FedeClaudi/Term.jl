3c1762ddf5f57e164ec03d344cccc441b3834e9c
FedeClaudi <federicoclaudi@protonmail.com>
2023-01-15 15:42:17 -0500

    revert

--- src/layout.jl
+++ src/layout.jl
@@ -511,8 +511,7 @@ function vstack(renderables::RenderablesUnion...; pad::Int = 0)
     return Renderable(segments, Measure(segments))
 end
 
-vstack(renderables::Union{AbstractVector,Tuple}; kwargs...) =
-    vstack(renderables...; kwargs...)
+vstack(renderables::Union{Vector,Tuple}; kwargs...) = vstack(renderables...; kwargs...)
 
 """
     hstack(r1::RenderablesUnion, r2::RenderablesUnion   )
@@ -563,6 +562,8 @@ function hstack(renderables...; pad::Int = 0)
     return renderable
 end
 
+hstack(renderables::Union{Vector,Tuple}; kwargs...) = hstack(renderables...; kwargs...)
+
 # --------------------------------- operators -------------------------------- #
 
 """
@@ -601,12 +602,9 @@ Right align renderables and then vertically stack.
 rvstack(renderables::RenderablesUnion...; kwargs...)::Renderable =
     vstack(rightalign(renderables...)...; kwargs...)
 
-rvstack(renderables::Union{Tuple,AbstractVector}; kwargs...) =
-    rvstack(renderables...; kwargs...)
-cvstack(renderables::Union{Tuple,AbstractVector}; kwargs...) =
-    cvstack(renderables...; kwargs...)
-lvstack(renderables::Union{Tuple,AbstractVector}; kwargs...) =
-    lvstack(renderables...; kwargs...)
+rvstack(renderables::Union{Tuple,Vector}; kwargs...) = rvstack(renderables...; kwargs...)
+cvstack(renderables::Union{Tuple,Vector}; kwargs...) = cvstack(renderables...; kwargs...)
+lvstack(renderables::Union{Tuple,Vector}; kwargs...) = lvstack(renderables...; kwargs...)
 
 # ---------------------------------------------------------------------------- #
 #                                LINES & SPACER                                #
